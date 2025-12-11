f3d97449c9cc58cdb3a18431183a27ad3abac8e2
Clara Wappling <clara.wappling@gmail.com>
2025-12-11 15:11:58 +0100

    clean up

--- clothing-recommendation.php
+++ clothing-recommendation.php
@@ -3,7 +3,5 @@
 get_header(); 
 ?>
 <?php echo do_shortcode('[test_weather]'); ?>
-<?php echo do_shortcode('[kids_clothes]'); ?>
-<?php echo do_shortcode('[kids_clothes_ai]'); ?>
 <?php echo do_shortcode('[kids_clothes_categories_icons]'); ?>
 <?php get_footer(); ?>
