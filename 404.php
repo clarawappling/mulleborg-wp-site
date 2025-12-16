870aa8ddec99d6f5bf9c04aef935bdee6c0565c3
Clara Wappling <clara.wappling@gmail.com>
2025-12-16 11:30:30 +0100

    basic img and style in place

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+    <div class="error-page">
+        <div class="error-image-wrapper">
+            <?php echo '<img src="' . get_template_directory_uri() . '/assets/images/Error_404_Mulle.png" alt="Custom Error 404 with Mulle">'; ?>
+        </div>
+        </div>
+
+<?php get_footer(); ?>
