0984656f3f65b4c28122cbee4e8e45bf49320816
Clara Wappling <clara.wappling@gmail.com>
2025-12-19 14:34:11 +0100

    OK version of sticky header with fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,3 +91,15 @@ window.addEventListener('scroll', () => {
 
 });
 
+
+    /* --------------------------------
+       HEADER SCROLL
+    ---------------------------------- */
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header-inner');
+    if(window.scrollY > 20){
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
