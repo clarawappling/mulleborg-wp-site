02babf12c583064ec081465b6f74529143bbd7f5
Clara Wappling <clara.wappling@gmail.com>
2026-01-18 22:47:49 +0100

    more sections for each template and correct logix for no divders by footer

--- cooperative.php
+++ cooperative.php
@@ -4,58 +4,165 @@ get_header();
 ?>
 
 <div class="cooperative-page">
-<div class=section-box>
-<?php if ($cooperative_page_image = get_field('cooperative_page_image')) : ?>
-        <div class="image-wrapper">
-            <img src="<?php echo esc_url($cooperative_page_image['url']); ?>" alt="<?php echo esc_attr($cooperative_page_image['alt']); ?>">
+
+    <?php
+    // FIELD VALUES
+    $image_1   = get_field('cooperative_page_image');
+    $heading_1 = get_field('cooperative_page_heading');
+    $text_1    = get_field('cooperative_page_text');
+
+    $image_2 = get_field('cooperative_page_image_2');
+    $text_2  = get_field('cooperative_page_text_2');
+
+    $image_3 = get_field('cooperative_page_image_3');
+    $text_3  = get_field('cooperative_page_text_3');
+
+    $image_4 = get_field('cooperative_page_image_4');
+    $text_4  = get_field('cooperative_page_text_4');
+    ?>
+
+    <!-- SECTION 1 -->
+    <?php if ($image_1 || $heading_1 || $text_1) : ?>
+        <div class="section-box">
+
+            <?php if ($image_1) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_1['url']); ?>" alt="<?php echo esc_attr($image_1['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($heading_1 || $text_1) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-orange">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex orange-bg info-box">
+                    <?php if ($heading_1) : ?>
+                        <h1><?php echo esc_html($heading_1); ?></h1>
+                    <?php endif; ?>
+
+                    <?php if ($text_1) : ?>
+                        <div><?php echo wp_kses_post($text_1); ?></div>
+                    <?php endif; ?>
+                </div>
+
+                <?php if ($image_2 || $text_2 || $image_3 || $text_3 || $image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-orange">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
         </div>
     <?php endif; ?>
 
-<?php 
-$cooperative_heading = get_field('cooperative_page_heading');
-$cooperative_text = get_field('cooperative_page_text');
 
-if ($cooperative_heading || $cooperative_text) : ?>
-<div class="section">
-    <div class="section-divider divider-to-orange">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-top.svg' ); ?>
-</div></div>
-    <div class="flex orange-bg info-box">
-        <?php if ($cooperative_heading) : ?>
-            <h1><?php echo esc_html($cooperative_heading); ?></h1>
-        <?php endif; ?>
+    <!-- SECTION 2 -->
+    <?php if ($image_2 || $text_2) : ?>
+        <div class="section-box">
 
-        <?php if ($cooperative_text) : ?>
-            <div><?php echo wp_kses_post($cooperative_text); ?></div>
-        <?php endif; ?>
-    </div>
-    <div class="section">
-    <div class="section-divider divider-to-orange">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg' ); ?>
-</div></div>
-<?php endif; ?>
+            <?php if ($image_2) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_2['url']); ?>" alt="<?php echo esc_attr($image_2['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_2) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-orange">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex orange-bg info-box">
+                    <div class="top-margin">
+                        <?php echo wp_kses_post($text_2); ?>
+                    </div>
+                </div>
+
+                <?php if ($image_3 || $text_3 || $image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-orange">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
+
+    <!-- SECTION 3 -->
+    <?php if ($image_3 || $text_3) : ?>
+        <div class="section-box">
+
+            <?php if ($image_3) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_3['url']); ?>" alt="<?php echo esc_attr($image_3['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_3) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-orange">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex orange-bg info-box">
+                    <div class="top-margin">
+                        <?php echo wp_kses_post($text_3); ?>
+                    </div>
+                </div>
+
+                <?php if ($image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-orange">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
+
+    <!-- SECTION 4 -->
+    <?php if ($image_4 || $text_4) : ?>
+        <div class="section-box">
+
+            <?php if ($image_4) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_4['url']); ?>" alt="<?php echo esc_attr($image_4['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_4) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-orange">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex orange-bg info-box">
+                    <div class="top-margin">
+                        <?php echo wp_kses_post($text_4); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
 </div>
-<div class=section-box>
-<?php if ($cooperative_page_image_2 = get_field('cooperative_page_image_2')) : ?>
-    <div class="image-wrapper">
-        <img src="<?php echo esc_url($cooperative_page_image_2['url']); ?>" alt="<?php echo esc_attr($cooperative_page_image_2['alt']); ?>">
-    </div>
-<?php endif; ?>
 
-<?php 
-$cooperative_page_text_2 = get_field('cooperative_page_text_2');
-
-if ($cooperative_page_text_2) : ?>
-<div class="section">
-    <div class="section-divider divider-to-orange">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-top.svg' ); ?>
-</div></div>
-    <div class="flex orange-bg info-box">
-        <?php if ($cooperative_page_text_2) : ?>
-            <div class="top-margin"><?php echo wp_kses_post($cooperative_page_text_2); ?></div>
-        <?php endif; ?>
-    </div> 
-<?php endif; ?>
-
-</div></div>
 <?php get_footer(); ?>
--- outdoors-page.php
+++ outdoors-page.php
@@ -2,77 +2,190 @@
 /* Template Name: Outdoors Page */
 get_header(); 
 ?>
+
 <div class="outdoors-page">
-<div class=section-box>
-<?php if ($outdoors_image = get_field('outdoors_image')) : ?>
-    <div class="image-wrapper">
-        <img src="<?php echo esc_url($outdoors_image['url']); ?>" alt="<?php echo esc_attr($outdoors_image['alt']); ?>">
-    </div>
-<?php endif; ?>
 
-<?php 
-$outdoors_heading = get_field('outdoors_heading');
-$outdoors_text_1 = get_field('outdoors_text_section_1');
+    <?php
+    // FIELD VALUES
+    $image_1   = get_field('outdoors_image');
+    $heading_1 = get_field('outdoors_heading');
+    $text_1    = get_field('outdoors_text_section_1');
 
-if ($outdoors_heading || $outdoors_text_1) : ?>
-<div class="section">
-    <div class="section-divider divider-to-green">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-top.svg' ); ?>
-</div></div>
-    <div class="flex green-bg info-box">
-        <?php if ($outdoors_heading) : ?>
-            <h1><?php echo esc_html($outdoors_heading); ?></h1>
-        <?php endif; ?>
+    $image_2 = get_field('outdoors_image_2');
+    $text_2  = get_field('outdoors_text_section_2');
+    $link_tip = get_field('outdoors_link_tip');
+    $ur_link  = get_field('i_ur_och_skur_link');
 
-        <?php if ($outdoors_text_1) : ?>
-            <div><?php echo wp_kses_post($outdoors_text_1); ?></div>
-        <?php endif; ?>
-    </div>
-    <div class="section">
-    <div class="section-divider divider-to-green">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg' ); ?>
-</div></div>
-<?php endif; ?>
+    $image_3 = get_field('outdoors_image_3');
+    $text_3  = get_field('outdoors_text_section_3');
+
+    $image_4 = get_field('outdoors_image_4');
+    $text_4  = get_field('outdoors_text_section_4');
+    ?>
+
+    <!-- SECTION 1 -->
+    <?php if ($image_1 || $heading_1 || $text_1) : ?>
+        <div class="section-box">
+
+            <?php if ($image_1) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_1['url']); ?>" alt="<?php echo esc_attr($image_1['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($heading_1 || $text_1) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-green">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex green-bg info-box">
+                    <?php if ($heading_1) : ?>
+                        <h1><?php echo esc_html($heading_1); ?></h1>
+                    <?php endif; ?>
+
+                    <?php if ($text_1) : ?>
+                        <div><?php echo wp_kses_post($text_1); ?></div>
+                    <?php endif; ?>
+                </div>
+
+                <?php if ($image_2 || $text_2 || $link_tip || $ur_link || $image_3 || $text_3 || $image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-green">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
+
+    <!-- SECTION 2 -->
+    <?php if ($image_2 || $text_2 || $link_tip || $ur_link) : ?>
+        <div class="section-box">
+
+            <?php if ($image_2) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_2['url']); ?>" alt="<?php echo esc_attr($image_2['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_2 || $link_tip || $ur_link) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-green">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex green-bg info-box">
+
+                    <?php if ($text_2) : ?>
+                        <div class="top-margin"><?php echo wp_kses_post($text_2); ?></div>
+                    <?php endif; ?>
+
+                    <?php if ($link_tip || $ur_link) : ?>
+                        <div>
+                            <?php if ($link_tip) : ?>
+                                <p><?php echo wp_kses_post($link_tip); ?></p>
+                            <?php endif; ?>
+
+                            <?php if ($ur_link) : ?>
+                                <p>
+                                    <a class="ius_brand-color-text"
+                                       href="<?php echo esc_url($ur_link); ?>"
+                                       target="_blank"
+                                       rel="noopener">
+                                        <?php echo esc_html($ur_link); ?>
+                                    </a>
+                                </p>
+                            <?php endif; ?>
+                        </div>
+                    <?php endif; ?>
+
+                </div>
+
+                <?php if ($image_3 || $text_3 || $image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-green">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
+
+    <!-- SECTION 3 -->
+    <?php if ($image_3 || $text_3) : ?>
+        <div class="section-box">
+
+            <?php if ($image_3) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_3['url']); ?>" alt="<?php echo esc_attr($image_3['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_3) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-green">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex green-bg info-box">
+                    <div class="top-margin">
+                        <?php echo wp_kses_post($text_3); ?>
+                    </div>
+                </div>
+
+                <?php if ($image_4 || $text_4) : ?>
+                    <div class="section">
+                        <div class="section-divider divider-to-green">
+                            <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-bottom.svg'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
+
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
+
+    <!-- SECTION 4 -->
+    <?php if ($image_4 || $text_4) : ?>
+        <div class="section-box">
+
+            <?php if ($image_4) : ?>
+                <div class="image-wrapper">
+                    <img src="<?php echo esc_url($image_4['url']); ?>" alt="<?php echo esc_attr($image_4['alt']); ?>">
+                </div>
+            <?php endif; ?>
+
+            <?php if ($text_4) : ?>
+                <div class="section">
+                    <div class="section-divider divider-to-green">
+                        <?php echo file_get_contents(get_template_directory() . '/assets/dividers/svgs/wave-top.svg'); ?>
+                    </div>
+                </div>
+
+                <div class="flex green-bg info-box">
+                    <div class="top-margin">
+                        <?php echo wp_kses_post($text_4); ?>
+                    </div>
+                </div>
+            <?php endif; ?>
+
+        </div>
+    <?php endif; ?>
+
 </div>
-<div class=section-box>
-<?php if ($outdoors_image_2 = get_field('outdoors_image_2')) : ?>
-    <div class="image-wrapper">
-        <img src="<?php echo esc_url($outdoors_image_2['url']); ?>" alt="<?php echo esc_attr($outdoors_image_2['alt']); ?>">
-    </div>
-<?php endif; ?>
 
-<?php 
-$outdoors_text_2 = get_field('outdoors_text_section_2');
-$outdoors_link_tip = get_field('outdoors_link_tip');
-$i_ur_link = get_field('i_ur_och_skur_link');
-
-if ($outdoors_text_2 || $outdoors_link_tip || $i_ur_link) : ?>
-<div class="section">
-    <div class="section-divider divider-to-green">
-    <?php echo file_get_contents( get_template_directory() . '/assets/dividers/svgs/wave-top.svg' ); ?>
-</div></div>
-    <div class="flex green-bg info-box">
-        <?php if ($outdoors_text_2) : ?>
-            <div class="top-margin"><?php echo wp_kses_post($outdoors_text_2); ?></div>
-        <?php endif; ?>
-
-        <?php if ($outdoors_link_tip || $i_ur_link) : ?>
-            <div>
-                <?php if ($outdoors_link_tip) : ?>
-                    <p><?php echo wp_kses_post($outdoors_link_tip); ?></p>
-                <?php endif; ?>
-
-                <?php if ($i_ur_link) : ?>
-                    <p>
-                        <a class="ius_brand-color-text" href="<?php echo esc_url($i_ur_link); ?>" target="_blank" rel="noopener">
-                            <?php echo esc_html($i_ur_link); ?>
-                        </a>
-                    </p>
-                <?php endif; ?>
-            </div>
-        <?php endif; ?>
-    </div> 
-<?php endif; ?>
-
-</div></div>
 <?php get_footer(); ?>
